def2cbe6d51029535d22652c41dd6af961a62692
Tzu-Ching Yang <shingekinocore@gmail.com>
2024-10-02 10:43:43 +0800

    Separate two colcon compilation

---
+++ src/custom/3-autoware/compile.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(realpath $(dirname $0))
+
+print_info "Download autoware"
+
+WD=$1
+
+cd $WD
+git clone https://github.com/autowarefoundation/autoware.git -b release/2024.03
+cd $WD/autoware
+
+print_info "Install autoware"
+
+./setup-dev-env.sh -y --no-nvidia
+
+# Compile autoware
+print_info "Preparing compilation environment of autoware"
+
+mkdir src
+# error may occur at the first time, run twice if needed may solve the issue
+vcs import src < autoware.repos || vcs import src < autoware.repos
+
+print_info "Autoware src prepared"
+
+# override buggy file
+cp $SCRIPT_DIR/package.xml \
+    $WD/autoware/src/universe/autoware.universe/evaluator/perception_online_evaluator
+
+print_info "Install ros packages"
+
+. /opt/ros/humble/setup.bash
+sudo apt-get update -y && sudo apt-get upgrade -y
+rosdep update
+rosdep install -y --from-paths src \
+                  --ignore-src \
+                  --rosdistro $ROS_DISTRO \
+                  > /dev/null 2>&1
+
+print_info "Autoware repo is ready, compile autoware"
+
+# prepare repo for compilation with different argument
+mkdir -p ../expr
+cp -r . ../expr/autoware
+
+# Run compilation
+colcon build --symlink-install \
+             --parallel-workers 32 \
+             --cmake-args \
+             -DCMAKE_BUILD_TYPE=Release &
+
+# Run compilation: experimental 
+# -DCMAKE_CUDA_ARCHITECTURES=native paramener
+cd ../expr/autoware
+colcon build --symlink-install \
+             --parallel-workers 32 \
+             --cmake-args \
+             -DCMAKE_CUDA_ARCHITECTURES=native \
+             -DCMAKE_BUILD_TYPE=Release &
+
+wait
+
+print_info "Autoware compiled successfully"
--- src/custom/3-autoware/setup.sh
+++ src/custom/3-autoware/setup.sh
@@ -2,46 +2,13 @@
 
 SCRIPT_DIR=$(realpath $(dirname $0))
 
-print_info "Download autoware"
+print_info "Prepare environment for compiling utoware"
 
 install_if_dne python3.10-venv apt-utils
-
-cd ~
-git clone https://github.com/autowarefoundation/autoware.git -b release/2024.03
-cd ~/autoware
-
-print_info "Install autoware"
 
 echo "export PATH=\$PATH:/home/shiritai/.local/bin" >> ~/.bashrc
 echo "export PATH=\$PATH:/home/shiritai/.local/bin" >> ~/.zshrc
 source ~/.bashrc
-
-./setup-dev-env.sh -y --no-nvidia
-
-# Compile autoware
-print_info "Preparing compilation environment of autoware"
-
-mkdir src
-# error may occur at the first time, run twice if needed may solve the issue
-vcs import src < autoware.repos || vcs import src < autoware.repos
-
-print_info "Autoware src prepared"
-
-# override buggy file
-cp $SCRIPT_DIR/package.xml \
-    ~/autoware/src/universe/autoware.universe/evaluator/perception_online_evaluator
-
-print_info "Install ros packages"
-
-. /opt/ros/humble/setup.bash
-sudo apt-get update -y && sudo apt-get upgrade -y
-rosdep update
-rosdep install -y --from-paths src \
-                  --ignore-src \
-                  --rosdistro $ROS_DISTRO \
-                  > /dev/null 2>&1
-
-print_info "Autoware repo is ready"
 
 print_info "Setup ccache..."
 
@@ -49,21 +16,18 @@ mkdir -p ~/.cache/ccache
 touch ~/.cache/ccache/ccache.conf
 echo "max_size = 60G" >> ~/.cache/ccache/ccache.conf
 
+echo "export CUDACXX=/usr/local/cuda/bin/nvcc" >> ~/.bashrc
 echo "export CC=/usr/lib/ccache/gcc" >> ~/.bashrc
 echo "export CXX=/usr/lib/ccache/g++" >> ~/.bashrc
 echo "export CCACHE_DIR=\${HOME}/.cache/ccache/" >> ~/.bashrc
 
+echo "export CUDACXX=/usr/local/cuda/bin/nvcc" >> ~/.zshrc
 echo "export CC=/usr/lib/ccache/gcc" >> ~/.zshrc
 echo "export CXX=/usr/lib/ccache/g++" >> ~/.zshrc
 echo "export CCACHE_DIR=\${HOME}/.cache/ccache/" >> ~/.zshrc
 
 print_info "Setup ccache successfully"
 
-# Run compilation
-print_info "Compile autoware"
-colcon build --symlink-install \
-             --parallel-workers 32 \
-             --cmake-args \
-             -DCMAKE_BUILD_TYPE=Release
+print_info "Autoware compilation environment is now set"
 
-print_info "Autoware compiled successfully"
+${SCRIPT_DIR}/compile.sh ~
