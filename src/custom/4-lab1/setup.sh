9c424aeb0542b42ee01b4ed20e03bf17833dd5b3
Tzu-Ching Yang <shingekinocore@gmail.com>
2024-10-02 09:14:15 +0800

    Add pip PATH

--- src/custom/4-lab1/setup.sh
+++ src/custom/4-lab1/setup.sh
@@ -3,5 +3,6 @@
 pip install gdown
 . ~/.bashrc
 
+export PATH=$PATH:/home/shiritai/.local/bin
 gdown -O ~/autoware_map/ 'https://docs.google.com/uc?export=download&id=1499_nsbUbIeturZaDj7jhUownh5fvXHd'
 unzip -d ~/autoware_map ~/autoware_map/sample-map-planning.zip
